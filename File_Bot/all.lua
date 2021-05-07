07407cde3bd1bd55893d0970861d903a90b68ee0
TELANDTEAM <82978925+TELANDTEAM@users.noreply.github.com>
2021-05-07 04:54:36 +0300

    Update all.lua

--- File_Bot/all.lua
+++ File_Bot/all.lua
@@ -25,7 +25,8 @@ tagname = tagname:gsub("[[]","")
 t = t..", ["..tagname.."](tg://user?id="..v.user_id_..")"
 if x == 5 or x == tags or k == 0 then
 local Text = t:gsub('#all,','#all\n')
-send(msg.chat_id_, 0,Text)
+---send(msg.chat_id_, 0,Text)
+sendText(msg.chat_id_,Text,msg.id_/2097152/0.5,'md')
 end
 end,nil)
 end
